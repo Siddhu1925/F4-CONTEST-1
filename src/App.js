52031ebe39e55d8a9df58d16379c9f70ec79bd43
Siddhu1925 <114770885+Siddhu1925@users.noreply.github.com>
2023-04-15 22:01:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="bg-black h-screen overflow-hidden w-screen  text-slate-200">
+      
+      
+       <Home/>
+    </div>
+  );
+}
+
+export default App;
